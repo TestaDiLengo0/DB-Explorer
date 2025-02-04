3494623669359adedce111ca25e7a2ba89971758
TestaDiLengo0 <168752469+TestaDiLengo0@users.noreply.github.com>
2025-02-04 22:04:10 +0300

    Добавлен поиск (очень кривой, но...); Удаление записей через контекстное меню; И новая кнопка.

--- DB Explorer v0.2/WorkPage.xaml.cs
+++ DB Explorer v0.2/WorkPage.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Data;
 using System.Configuration;
 using Npgsql;
+using System.Windows.Documents.DocumentStructures;
 
 namespace DB_Explorer_v0._2
 {
@@ -57,9 +58,29 @@ namespace DB_Explorer_v0._2
 
     public class Makers
     {
-        private static string connectionString = ConfigurationManager.ConnectionStrings["university_DB"].ToString();
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["university_DB"].ToString();
 
-        static public DataTable makeTableByQuerry(string Querry)
+        //
+        //Method that makes the SQL-Querry by some arguments
+        //
+
+        static public String MakeQuerry(string tableName, string targetColumn, string[] args)
+        {
+            string querry = $"SELECT * FROM {tableName}";
+            if (args.Length == 0 | args == null) return querry;
+            querry += $" WHERE {targetColumn} LIKE \'%{args[0]}%\'";
+            for (int i = 1; i < args.Length; i++)
+            {
+                querry += $" OR {targetColumn} LIKE \'%{args[i]}%\'";
+            }
+            return querry;
+        }
+
+        //
+        //Методы для создания таблиц для наполнения Грида
+        //
+
+        static public DataTable MakeTableByQuerry(string Querry)
         {
             NpgsqlConnection connection = new NpgsqlConnection(connectionString);
             connection.Open();
@@ -79,67 +100,111 @@ namespace DB_Explorer_v0._2
             return dt;
         } 
 
-        static public DataTable makeTeachersTable(string Querry)
+        static public DataTable MakeTeachersTable(string Querry)
         {
             //Создание таблицы
-            DataTable table = makeTableByQuerry(Querry);
+            DataTable table = MakeTableByQuerry(Querry);
 
             //Локализация имен столбцов
-            table.Columns[0].ColumnName = "ID";
-            table.Columns[1].ColumnName = "Имя";
-            table.Columns[2].ColumnName = "ID кафедры";
+            if (table.Rows.Count > 0)
+            {
+                table.Columns[0].ColumnName = "ID";
+                table.Columns[1].ColumnName = "Имя";
+                table.Columns[2].ColumnName = "ID кафедры";
+            }
+            else
+            {
+                table.Columns.Add("Вывод");
+                table.Rows.Add("По этому запросу ничего не найдено");
+            }
             return table;
         }
 
-        static public DataTable makeMaterialsTable(string Querry)
+        static public DataTable MakeMaterialsTable(string Querry)
         {
-            DataTable table = makeTableByQuerry(Querry);
+            DataTable table = MakeTableByQuerry(Querry);
 
-            table.Columns[0].ColumnName = "ID";
-            table.Columns[1].ColumnName = "Название";
-            table.Columns[2].ColumnName = "ID Автора";
-            table.Columns[3].ColumnName = "Год выхода";
-            table.Columns[4].ColumnName = "Число страниц";
-            table.Columns[5].ColumnName = "ID типа издания";
-            table.Columns[6].ColumnName = "ID дисциплины";
-
+            if (table.Rows.Count > 0)
+            {
+                table.Columns[0].ColumnName = "ID";
+                table.Columns[1].ColumnName = "Название";
+                table.Columns[2].ColumnName = "ID Автора";
+                table.Columns[3].ColumnName = "Год выхода";
+                table.Columns[4].ColumnName = "Число страниц";
+                table.Columns[5].ColumnName = "ID типа издания";
+                table.Columns[6].ColumnName = "ID дисциплины";
+            }
+            else
+            {
+                table.Columns.Add("Вывод");
+                table.Rows.Add("По этому запросу ничего не найдено");
+            }
             return table;
         }
 
-        static public DataTable makeDisciplinesTable(string Querry) 
+        static public DataTable MakeDisciplinesTable(string Querry) 
         {
-            DataTable table = makeTableByQuerry(Querry);
+            DataTable table = MakeTableByQuerry(Querry);
 
-            table.Columns[0].ColumnName = "ID";
-            table.Columns[1].ColumnName = "Название дисциплины";
-
+            if (table.Rows.Count > 0)
+            {
+                table.Columns[0].ColumnName = "ID";
+                table.Columns[1].ColumnName = "Название дисциплины";
+            }
+            else
+            {
+                table.Columns.Add("Вывод");
+                table.Rows.Add("По этому запросу ничего не найдено");
+            }
             return table;
         }
 
-        static public DataTable makeDepartmentsTable(string Querry)
+        static public DataTable MakeDepartmentsTable(string Querry)
         {
-            DataTable table = makeTableByQuerry(Querry);
+            DataTable table = MakeTableByQuerry(Querry);
 
-            table.Columns[0].ColumnName = "ID";
-            table.Columns[1].ColumnName = "Название кафедры";
-
+            if (table.Rows.Count > 0)
+            {
+                table.Columns[0].ColumnName = "ID";
+                table.Columns[1].ColumnName = "Название кафедры";
+            }
+            else
+            {
+                table.Columns.Add("Вывод");
+                table.Rows.Add("По этому запросу ничего не найдено");
+            }
             return table;
         }
 
-        static public DataTable makeTypesOfMaterialsTable(string Querry)
+        static public DataTable MakeTypesOfMaterialsTable(string Querry)
         {
-            DataTable table = makeTableByQuerry(Querry);
+            DataTable table = MakeTableByQuerry(Querry);
 
-            table.Columns[0].ColumnName = "ID";
-            table.Columns[1].ColumnName = "Название типа";
-
+            if (table.Rows.Count > 0)
+            {
+                table.Columns[0].ColumnName = "ID";
+                table.Columns[1].ColumnName = "Название типа";
+            }
+            else
+            {
+                table.Columns.Add("Вывод");
+                table.Rows.Add("По этому запросу ничего не найдено");
+            }
             return table;
         }
+        //////////////////////////////////////////////////////////////
+        ///
+
+        //
+        //Методы для создания окон редактирования и вставки данных
+        // Нужно будет ещё это обдумать
+        //
+
     }
 
     public partial class WorkPage : Page
     {
-        enum Tables
+        public enum Tables
         {
             teachers,
             guides_types,
@@ -148,15 +213,23 @@ namespace DB_Explorer_v0._2
             departments
         }
 
-        static private string connectionString = ConfigurationManager.ConnectionStrings["university_DB"].ToString();
-        static private Tables currentTable;
+        static public Tables currentTable;
+
+
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["university_DB"].ToString();
+
+        private string teachersEnter = string.Empty;
+        private string materialsEnter = string.Empty;
+        private string disciplinesEnter = string.Empty;
+        private string departmentsEnter = string.Empty;
+        private string typesEnter = string.Empty;
 
         public WorkPage()
         {
             InitializeComponent();
 
 
-            SetDataGrid(Makers.makeTeachersTable("SELECT * FROM teachers"));
+            SetDataGrid(Makers.MakeTeachersTable("SELECT * FROM teachers"));
             currentTable = Tables.teachers;
         }
 
@@ -174,8 +247,15 @@ namespace DB_Explorer_v0._2
             Departments.Foreground = new SolidColorBrush(Colors.Black);
             TypesOfMaterials.Foreground = new SolidColorBrush(Colors.Black);
 
-            string Querry = "SELECT * FROM teachers";
-            SetDataGrid(Makers.makeTeachersTable(Querry));
+            if (teachersEnter == string.Empty)
+            {
+                string Querry = "SELECT * FROM teachers";
+                SetDataGrid(Makers.MakeTeachersTable(Querry));
+            }
+            currentTable = Tables.teachers;
+
+            SearchBox.Text = teachersEnter;
+            MaterialDesignThemes.Wpf.HintAssist.SetHelperText(SearchBox, "Поиск по имени преподавателя");
         }
 
         private void TablesMenuMaterials_Click(object sender, RoutedEventArgs e)
@@ -187,8 +267,15 @@ namespace DB_Explorer_v0._2
             Departments.Foreground = new SolidColorBrush(Colors.Black);
             TypesOfMaterials.Foreground = new SolidColorBrush(Colors.Black);
 
-            string Querry = "SELECT * FROM guides";
-            SetDataGrid(Makers.makeMaterialsTable(Querry));
+            if (materialsEnter == string.Empty)
+            {
+                string Querry = "SELECT * FROM guides";
+                SetDataGrid(Makers.MakeMaterialsTable(Querry));
+            }
+            currentTable = Tables.guides;
+
+            SearchBox.Text = materialsEnter;
+            MaterialDesignThemes.Wpf.HintAssist.SetHelperText(SearchBox, "Поиск по названию издания");
         }
 
         private void TablesMenuDisciplines_Click(object sender, RoutedEventArgs e)
@@ -200,8 +287,15 @@ namespace DB_Explorer_v0._2
             Departments.Foreground = new SolidColorBrush(Colors.Black);
             TypesOfMaterials.Foreground = new SolidColorBrush(Colors.Black);
 
-            string Querry = "SELECT * FROM disciplines";
-            SetDataGrid(Makers.makeDisciplinesTable(Querry));
+            if (disciplinesEnter == string.Empty)
+            {
+                string Querry = "SELECT * FROM disciplines";
+                SetDataGrid(Makers.MakeDisciplinesTable(Querry));
+            }
+            currentTable = Tables.disciplines;
+
+            SearchBox.Text = disciplinesEnter;
+            MaterialDesignThemes.Wpf.HintAssist.SetHelperText(SearchBox, "Поиск по названию дисциплины");
         }
 
         private void TablesMenuDepartments_Click(object sender, RoutedEventArgs e)
@@ -213,8 +307,15 @@ namespace DB_Explorer_v0._2
             Departments.Foreground = new SolidColorBrush(Colors.Purple);
             TypesOfMaterials.Foreground = new SolidColorBrush(Colors.Black);
 
-            string Querry = "SELECT * FROM departments";
-            SetDataGrid(Makers.makeDepartmentsTable(Querry));
+            if (departmentsEnter == string.Empty)
+            {
+                string Querry = "SELECT * FROM departments";
+                SetDataGrid(Makers.MakeDepartmentsTable(Querry));
+            }
+            currentTable = Tables.departments;
+
+            SearchBox.Text = departmentsEnter;
+            MaterialDesignThemes.Wpf.HintAssist.SetHelperText(SearchBox, "Поиск по названию кафедры");
         }
 
         private void TablesMenuTypesOfMaterials_Click(object sender, RoutedEventArgs e)
@@ -226,13 +327,112 @@ namespace DB_Explorer_v0._2
             Departments.Foreground = new SolidColorBrush(Colors.Black);
             TypesOfMaterials.Foreground = new SolidColorBrush(Colors.Purple);
 
-            string Querry = "SELECT * FROM guides_types";
-            SetDataGrid(Makers.makeTypesOfMaterialsTable(Querry));
+            if (typesEnter == string.Empty)
+            {
+                string Querry = "SELECT * FROM guides_types";
+                SetDataGrid(Makers.MakeTypesOfMaterialsTable(Querry));
+            }
+            currentTable = Tables.guides_types;
+
+            SearchBox.Text = typesEnter;
+            MaterialDesignThemes.Wpf.HintAssist.SetHelperText(SearchBox, "Поиск по типам");
+        }
+
+        private DataTable CreateTableWithEnters()
+        {
+            string args;
+            string targetColumn;
+            DataTable dt = new DataTable();
+            switch (currentTable)
+            {
+                case Tables.teachers:
+                    teachersEnter = SearchBox.Text;
+                    args = teachersEnter;
+                    targetColumn = "teacher_name";
+
+                    dt = Makers.MakeTeachersTable(Makers.MakeQuerry(currentTable.ToString(), targetColumn, args.Split(' ')));
+                    break;
+                case Tables.guides:
+                    materialsEnter = SearchBox.Text;
+                    args = materialsEnter;
+                    targetColumn = "guide_name";
+
+                    dt = Makers.MakeMaterialsTable(Makers.MakeQuerry(currentTable.ToString(), targetColumn, args.Split(' ')));
+                    break;
+                case Tables.disciplines:
+                    disciplinesEnter = SearchBox.Text;
+                    args = disciplinesEnter;
+                    targetColumn = "discipline_name";
+
+                    dt = Makers.MakeDisciplinesTable(Makers.MakeQuerry(currentTable.ToString(), targetColumn, args.Split(' ')));
+                    break;
+                case Tables.departments:
+                    departmentsEnter = SearchBox.Text;
+                    args = departmentsEnter;
+                    targetColumn = "department_name";
+
+                    dt = Makers.MakeDepartmentsTable(Makers.MakeQuerry(currentTable.ToString(), targetColumn, args.Split(' ')));
+                    break;
+                case Tables.guides_types:
+                    typesEnter = SearchBox.Text;
+                    args = typesEnter;
+                    targetColumn = "guide_tye_name";
+
+                    dt = Makers.MakeTypesOfMaterialsTable(Makers.MakeQuerry(currentTable.ToString(), targetColumn, args.Split(' ')));
+                    break;
+            }
+            return dt;
         }
 
         private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            SetDataGrid(CreateTableWithEnters());
+        }
 
+        private void ContextDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Это действие удалит также все связанные записи!\nХотите продолжить?", "Вопрос жизни и смерти", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                int id = (int)((DataRowView)DBDataGrid.SelectedItem)["ID"];
+                NpgsqlConnection connection = new(connectionString);
+                connection.Open();
+
+                NpgsqlCommand command = new();
+                command.Connection = connection;
+                command.CommandText = $"DELETE FROM {currentTable.ToString()} WHERE {currentTable.ToString()}_id = {id}";
+                command.ExecuteNonQuery();
+
+                SetDataGrid(CreateTableWithEnters());
+            }
+        }
+
+        private void ContextUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddNewRow_Click(object sender, RoutedEventArgs e)
+        {
+            switch(currentTable)
+            {
+                case Tables.teachers:
+
+                    break;
+                case Tables.guides:
+
+                    break;
+                case Tables.disciplines:
+
+                    break;
+                case Tables.departments:
+
+                    break;
+                case Tables.guides_types:
+
+                    break;
+            }
         }
     }
 }
